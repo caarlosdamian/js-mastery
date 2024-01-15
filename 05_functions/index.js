82d1eb905ec9b9aca8700c8ac7a57ae5e1f0eb0b
Carlos Damian <carlos.damian@choicehotels.com>
2024-01-15 14:28:39 -0600

    merge main

---
+++ 05_functions/index.js
@@ -0,0 +1,41 @@
+// block of code -> performs a task
+
+// // A function declarations
+// function square(number) {
+//   return number * number;
+// }
+
+// // function call
+// const result = square(3);
+// console.log(result);
+
+// function declaration
+// // have access to 'this' keyword
+// function name(params) {
+//   // statements
+// }
+
+// // function expression
+// // anonymous function no name
+// const name2 = function (params) {
+//   // statemens
+// };
+
+// // arrow function
+// // modern way
+// const nameFunction = (params) => {};
+
+// function saytHi(name) {
+//   console.log(`Hi, ${name}`);
+// }
+
+// saytHi('Carlos');
+// every funtions returns undefined
+
+function add(a, b) {
+  return a + b;
+}
+
+const sum = add(2, 2);
+
+console.log(sum);
