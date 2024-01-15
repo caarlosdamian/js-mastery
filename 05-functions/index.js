69ff1eb0c5537cabb96b11fa4c329bdf23a985d9
caarlosdamian <dapecarlos@gmail.com>
2024-01-15 11:46:02 -0600

    feat(functions): added frunctions

---
+++ 05-functions/index.js
@@ -0,0 +1,24 @@
+// // aroow functions
+
+// const square = (number) => {
+//   return number * number;
+// };
+
+// const square1 = (number) => number * number;
+
+// const result = square(5);
+
+// console.log(result);
+
+// // parametros -> cuando se define una funcion
+
+// // argumentos -> variables reales que se pasan cuando se hace una llamada a una funcion
+
+// const sayHi = (name) => {
+//   console.log(`Hello , ${name}`);
+// };
+
+// sayHi('John');
+
+// //
+
